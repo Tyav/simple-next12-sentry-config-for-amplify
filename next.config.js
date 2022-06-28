a560b507b073567d558bb237d7343c0a46d2a399
Tyav <tyav2greenz@gmail.com>
2022-06-28 14:10:29 +0100

    add

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const moduleExports = {
   //   disableServerWebpackPlugin: true,
   //   disableClientWebpackPlugin: true,
   // },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 const sentryWebpackPluginOptions = {
