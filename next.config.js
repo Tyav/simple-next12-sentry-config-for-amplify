e40942f52a926b4862caba7f7b660ea37bd3739e
Tyav <tyav2greenz@gmail.com>
2022-06-28 10:48:15 +0100

    add webpack

--- next.config.js
+++ next.config.js
@@ -25,6 +25,7 @@ const sentryWebpackPluginOptions = {
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
+
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
