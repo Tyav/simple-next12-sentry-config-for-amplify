70df0598c1fa22be9bc9243535063bad52e6b2b6
Tyav <tyav2greenz@gmail.com>
2022-06-29 09:34:33 +0100

    yarn s

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// const { withSentryConfig } = require('@sentry/nextjs');
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const moduleExports = {
   // Your existing module.exports
@@ -13,11 +13,16 @@ const moduleExports = {
   //   disableServerWebpackPlugin: true,
   //   disableClientWebpackPlugin: true,
   // },
+  target: 'experimental-serverless-trace',
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.=
     ignoreDuringBuilds: true,
-  }
+  },
+  // webpack: (data1, data2) => {
+  //   console.log({data1, data2})
+  // }
+  // 
 };
 
 const sentryWebpackPluginOptions = {
@@ -35,4 +40,4 @@ const sentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = /* withSentryConfig( */moduleExports /*, sentryWebpackPluginOptions); */
+module.exports = withSentryConfig( moduleExports, sentryWebpackPluginOptions);
