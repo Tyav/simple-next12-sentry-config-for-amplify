31bc208f68c123f098e9b66d9d7aad2f3d0e3676
Tyav <tyav2greenz@gmail.com>
2022-07-05 01:44:50 +0100

    fic

--- next.config-don.js
+++ next.config-don.js
@@ -1,13 +1,25 @@
-
+/** @type {import('next').NextConfig} */
 // This file sets a custom webpack configuration to use your Next.js app
 // with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs');
-
 const moduleExports = {
   // Your existing module.exports
+  reactStrictMode: true,
+  // sentry: {
+  //   disableServerWebpackPlugin: true,
+  //   disableClientWebpackPlugin: true,
+  // },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.=
+    ignoreDuringBuilds: true,
+  },
+  // webpack: (data1, data2) => {
+  //   console.log({data1, data2})
+  // }
+  // 
 };
 
 const sentryWebpackPluginOptions = {
@@ -20,8 +32,10 @@ const sentryWebpackPluginOptions = {
   silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
+
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = moduleExports;
+// module.exports = withSentryConfig( moduleExports, sentryWebpackPluginOptions);
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
-// import ErrorBoundary from '../components/ErrorBoundary'
-import { ErrorBoundary } from 'react-error-boundary'
+import ErrorBoundary from '../components/Boundary'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps, err }) {
-  return (<ErrorBoundary>
-  <Component {...pageProps} />
-</ErrorBoundary>)
+function MyApp({ Component, pageProps, ...err }) {
+  console.log(err)
+  return (<ErrorBoundary FallbackComponent={
+    () => (<p> Error rendered</p>)
+  }>
+    <Component {...pageProps} err={err.err}/>
+  </ErrorBoundary>
+)
 }
 
 export default MyApp
