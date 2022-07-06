1f106e9bf4009c3df3ec1f194d182218ac8a7b1a
Tyav <tyav2greenz@gmail.com>
2022-07-06 06:00:45 +0100

    update error file

--- pages/_error.js
+++ pages/_error.js
@@ -1,57 +1,69 @@
-import NextErrorComponent from 'next/error';
+import NextErrorComponent from 'next/error'
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs'
+import Link from "next/link";
+import styled from "styled-components";
 
-const MyError = ({ statusCode, hasGetInitialPropsRun, err }) => {
-  if (!hasGetInitialPropsRun && err) {
-    // getInitialProps is not called in case of
-    // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
-    // err via _app.js so it can be captured
-    Sentry.captureException(err);
-    // Flushing is not required in this case as it only happens on the client
-  }
+function ErrorMessage({ statusText, message }) {
+  return (
+    <ErrorMessageWrapper>
+      <ErrorCode>{statusText}</ErrorCode>
+      {message && <ErrorDescription>{message}</ErrorDescription>}
+    </ErrorMessageWrapper>
+  );
+}
 
-  return <NextErrorComponent statusCode={statusCode} />;
-};
+export function Error({ statusCode }) {
+  return (
+    <Wrapper>
+      <Main>
+        <CenterContainer>
+          <h1>
+            Oops! We can&apos;t seem to find the page you&apos;re looking for.
+          </h1>
+          <div>
+            <ErrorMessage
+              message={
+                statusCode === 404
+                  ? ""
+                  : "Something went wrong. No worries you didn't break anything. We are working to fix it."
+              }
+              statusText={"Page Not Found"}
+            />
+          </div>
+          <h2>
+            Looks like you&apos;re on the wrong side of town, buddy. Let&apos;s
+            get you back on the <Link href="/">learning path</Link>.
+          </h2>
+        </CenterContainer>{" "}
+      </Main>
+    </Wrapper>
+  );
+}
 
-MyError.getInitialProps = async (context) => {
-  const errorInitialProps = await NextErrorComponent.getInitialProps(context);
-  
-  const { res, err, asPath } = context;
+Error.getInitialProps = async ({ res, err, asPath }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  const errorInitialProps = await NextErrorComponent.getInitialProps({
+    res,
+    err,
+  })
+  errorInitialProps.hasGetInitialPropsRun = true
 
-  // Workaround for https://github.com/vercel/next.js/issues/8592, mark when
-  // getInitialProps has run
-  errorInitialProps.hasGetInitialPropsRun = true;
-
-  // Returning early because we don't want to log 404 errors to Sentry.
   if (res?.statusCode === 404) {
     return errorInitialProps;
   }
-  
-  // Running on the server, the response object (`res`) is available.
-  //
-  // Next.js will pass an err on the server if a page's data fetching methods
-  // threw or returned a Promise that rejected
-  //
-  // Running on the client (browser), Next.js will provide an err if:
-  //
-  //  - a page's `getInitialProps` threw or returned a Promise that rejected
-  //  - an exception was thrown somewhere in the React lifecycle (render,
-  //    componentDidMount, etc) that was caught by Next.js's React Error
-  //    Boundary. Read more about what types of exceptions are caught by Error
-  //    Boundaries: https://reactjs.org/docs/error-boundaries.html
 
   if (err) {
-    Sentry.captureException(err);
+    Sentry.captureException(err)
 
     // Flushing before returning is necessary if deploying to Vercel, see
     // https://vercel.com/docs/platform/limits#streaming-responses
-    await Sentry.flush(2000);
+    await Sentry.flush(2000)
 
-    return errorInitialProps;
+    return errorInitialProps
   }
 
-  // If this point is reached, getInitialProps was called without any
+    // If this point is reached, getInitialProps was called without any
   // information about what the error might be. This is unexpected and may
   // indicate a bug introduced in Next.js, so record it in Sentry
   Sentry.captureException(
@@ -59,7 +71,59 @@ MyError.getInitialProps = async (context) => {
   );
   await Sentry.flush(2000);
 
-  return errorInitialProps;
+  return { statusCode, ...errorInitialProps };
 };
 
-export default MyError;
+const ErrorMessageWrapper = styled.div`
+  padding: 5rem 2rem;
+  border: 1px solid var(--clr-primary-600);
+  border-radius: 1rem;
+  margin: 2rem 0;
+`;
+
+const ErrorCode = styled.h3`
+  font-size: 5rem;
+  font-weight: 700;
+`;
+const ErrorDescription = styled.p``;
+
+const Section = styled.section`
+  padding: 66px 0 50px 0;
+`;
+
+const Main = styled.main`
+  display: flex;
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  padding: 16px 16px 20px;
+  @media (max-width: 450px) {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+  main {
+    max-width: 980px;
+    margin: 0 auto;
+  }
+`;
+
+const CenterContainer = styled.div`
+  position: relative;
+  width: 90vw;
+  height: 90vh;
+  text-align: center;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h2 {
+    max-width: 700px;
+    a {
+      color: var(--clr-primary-600);
+    }
+  }
+`;
+
+export default Error;
